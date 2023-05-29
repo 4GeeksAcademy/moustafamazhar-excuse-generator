bcfcf4796364cac42082fe1bbf9a7fd01530c2a4
Mostafa Mazhar <moustafammazhar@gmail.com>
2023-05-29 10:49:22 -0400

    added #id excuse to h2 and arrow => generateExuse

--- src/app.js
+++ src/app.js
@@ -4,7 +4,22 @@ import "./style.css";
 
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello World!");
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  return "My dog Ate my Food";
 };
